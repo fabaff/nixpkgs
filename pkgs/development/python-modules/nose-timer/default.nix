297e34e200cf532b55e4f6fae5f917b3c67cbd14
Fabian Affolter <mail@fabian-affolter.ch>
2021-03-09 17:54:27 +0100

    python3Packages.nose-timer: enable tests

--- pkgs/development/python-modules/nose-timer/default.nix
+++ pkgs/development/python-modules/nose-timer/default.nix
@@ -1,20 +1,42 @@
-{ buildPythonPackage, fetchPypi, lib, nose, }:
+{ lib
+, buildPythonPackage
+, fetchFromGitHub
+, nose
+, mock
+, parameterized
+, termcolor
+}:
 
 buildPythonPackage rec {
   pname = "nose-timer";
   version = "1.0.1";
 
-  src = fetchPypi {
-    inherit pname version;
-    sha256 = "sha256-j3DRA7f/2RIqWJ3g350Den2WdRm/beEiYh0hhmCbnjo=";
+  src = fetchFromGitHub {
+    owner = "mahmoudimus";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "0xsai2l5i1av62y9y0q63wy2zk27klmf2jizgghhxg2y8nfa8x3x";
   };
 
   propagatedBuildInputs = [ nose ];
 
+  checkInputs = [
+    mock
+    nose
+    parameterized
+    termcolor
+  ];
+
+  checkPhase = ''
+    nosetests --verbosity 2 tests
+  '';
+
+  pythonImportsCheck = [ "nosetimer" ];
+
   meta = with lib; {
+    description = "A timer plugin for nosetests";
     homepage = "https://github.com/mahmoudimus/nose-timer";
     license = licenses.mit;
-    description = "A timer plugin for nosetests (how much time does every test take?)";
     maintainers = with maintainers; [ doronbehar ];
   };
 }
