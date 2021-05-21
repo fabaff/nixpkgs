bcce9a692f9c624cad2b1752700414e113d07546
Andrew Brooks <andrew.brooks@flightaware.com>
2021-05-21 17:38:52 -0500

    tcltls: 1.6.7 -> 1.7.21
    Upgrade tcltls and use new tcl-lang.org homepage.
    This version no longer needs to be built against openssl_1_0_2.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -18295,9 +18295,7 @@ in
 
   tcllib = callPackage ../development/libraries/tcllib { };
 
-  tcltls = callPackage ../development/libraries/tcltls {
-    openssl = openssl_1_0_2;
-  };
+  tcltls = callPackage ../development/libraries/tcltls { };
 
   tclx = callPackage ../development/libraries/tclx { };
 
