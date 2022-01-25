9368baadfba97a8b409eb9cd465db9fc7ecb159b
Fabian Affolter <mail@fabian-affolter.ch>
2022-01-25 21:56:13 +0100

    python3Packages.dsmr-parser: 0.31 -> 0.32

--- pkgs/development/python-modules/dsmr-parser/default.nix
+++ pkgs/development/python-modules/dsmr-parser/default.nix
@@ -11,7 +11,7 @@
 
 buildPythonPackage rec {
   pname = "dsmr-parser";
-  version = "0.31";
+  version = "0.32";
   format = "setuptools";
 
   disabled = pythonOlder "3.8";
@@ -20,7 +20,7 @@ buildPythonPackage rec {
     owner = "ndokter";
     repo = "dsmr_parser";
     rev = "v${version}";
-    sha256 = "sha256-A+fyAAV62Dg28RDiivKEjrC1bCXzkuIvpATR+oUytRU=";
+    sha256 = "sha256-NFnFeWnXhRFqV75Tz4B4y0dz+mB3OX218uXqytpLJkI=";
   };
 
   propagatedBuildInputs = [
