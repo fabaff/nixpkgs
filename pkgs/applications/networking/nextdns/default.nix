2dc7eb0b9bb57a0868bb64f3d0fb12c63fc61257
Fabian Affolter <mail@fabian-affolter.ch>
2024-04-28 10:01:47 +0200

    nextdns: refactor

--- pkgs/applications/networking/nextdns/default.nix
+++ pkgs/applications/networking/nextdns/default.nix
@@ -1,4 +1,8 @@
-{ buildGoModule, fetchFromGitHub, lib }:
+{
+  lib,
+  buildGoModule,
+  fetchFromGitHub,
+}:
 
 buildGoModule rec {
   pname = "nextdns";
@@ -7,17 +11,22 @@ buildGoModule rec {
   src = fetchFromGitHub {
     owner = "nextdns";
     repo = "nextdns";
-    rev = "v${version}";
-    sha256 = "sha256-aQUz6FK04h3nzieK9fX7odVVt/zcdhXlX3T1Z1rN/ak=";
+    rev = "refs/tags/v${version}";
+    hash = "sha256-aQUz6FK04h3nzieK9fX7odVVt/zcdhXlX3T1Z1rN/ak=";
   };
 
   vendorHash = "sha256-DATSGSFRMrX972CWCiSIlOhDuAG3zcVyuILZ3IpVirM=";
 
-  ldflags = [ "-s" "-w" "-X main.version=${version}" ];
+  ldflags = [
+    "-s"
+    "-w"
+    "-X=main.version=${version}"
+  ];
 
   meta = with lib; {
     description = "NextDNS DNS/53 to DoH Proxy";
     homepage = "https://nextdns.io";
+    changelog = "https://github.com/nextdns/nextdns/releases/tag/v${version}";
     license = licenses.mit;
     maintainers = with maintainers; [ pnelson ];
     mainProgram = "nextdns";
