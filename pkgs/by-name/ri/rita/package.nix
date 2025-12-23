5c86423c86e6a14e9ac78135b109690e5c6ae93c
Fabian Affolter <mail@fabian-affolter.ch>
2025-12-23 23:52:38 +0100

    rita: 4.8.1 -> 5.1.0
    Changelog: https://github.com/activecm/rita/releases/tag/v5.1.0

--- pkgs/by-name/ri/rita/package.nix
+++ pkgs/by-name/ri/rita/package.nix
@@ -6,16 +6,16 @@
 
 buildGoModule rec {
   pname = "rita";
-  version = "4.8.1";
+  version = "5.1.0";
 
   src = fetchFromGitHub {
     owner = "activecm";
     repo = "rita";
     tag = "v${version}";
-    hash = "sha256-By0JvQ4LTm+NEnRMadE1x2PiiYqnJQCsF3Fy+gHulXs=";
+    hash = "sha256-+GVjjMwhPePyVaicwyJEImaf+Yd8VzRJfqp0gxOranI=";
   };
 
-  vendorHash = "sha256-KyC7VPgWlgKD6KWWRo3hFQHl2HjTub+VSMtJCpYE6Zk=";
+  vendorHash = "sha256-7QjrQHkyEYX1WkSuK3u0JVVVeYlAXalXnCsI3rZcJr4=";
 
   ldflags = [
     "-s"
@@ -24,10 +24,25 @@ buildGoModule rec {
     "-X=github.com/activecm/rita/config.ExactVersion=${version}"
   ];
 
+  doCheck = false;
+
+  preCheck = ''
+    # Test requires network access
+    substituteInPlace cmd/cmd_test.go \
+      --replace-fail "TestCheckForUpdate" "Skip_TestCheckForUpdate"
+    substituteInPlace database/threat_intel_test.go \
+      --replace-fail "TestParseOnlineFeeds" "Skip_TestParseOnlineFeeds"
+    substituteInPlace database/server_test.go \
+      --replace-fail "TestDatabase" "Skip_TestDatabase"
+    # Test wants use Docker
+    substituteInPlace cmd/cmd_test.go \
+      --replace-fail "TestCmdTestSuite" "Skip_TestCmdTestSuite"
+  '';
+
   meta = {
     description = "Framework for detecting command and control communication through network traffic analysis";
     homepage = "https://github.com/activecm/rita";
-    changelog = "https://github.com/activecm/rita/releases/tag/v${version}";
+    changelog = "https://github.com/activecm/rita/releases/tag/${src.tag}";
     license = lib.licenses.gpl3Only;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "rita";
