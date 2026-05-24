7b3d4fb25aa25dceb861064fe6da541657657bc5
Fabian Affolter <mail@fabian-affolter.ch>
2026-05-24 23:06:30 +0200

    python3Packages.async-upnp-client: 0.46.2 -> 0.47.0
    Diff: https://github.com/StevenLooman/async_upnp_client/compare/0.46.2...0.47.0
    
    Changelog: https://github.com/StevenLooman/async_upnp_client/blob/0.47.0/CHANGES.rst

--- pkgs/development/python-modules/async-upnp-client/default.nix
+++ pkgs/development/python-modules/async-upnp-client/default.nix
@@ -21,14 +21,14 @@
 
 buildPythonPackage rec {
   pname = "async-upnp-client";
-  version = "0.46.2";
+  version = "0.47.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "StevenLooman";
     repo = "async_upnp_client";
     tag = version;
-    hash = "sha256-KJiEfu+JKDycBT14gFK4sBFCG3TN61DZEDth9y6CHp4=";
+    hash = "sha256-64PF+3DEBOI1fu/jA43hrY4uxm0fas/2B2/o28Rhfao=";
   };
 
   build-system = [ setuptools ];
